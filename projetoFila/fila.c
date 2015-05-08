c28fa34ad550877cced2fee968771f0c28836346
jaici <jaicimara.w@aluno.ifsc.edu.br>
2015-05-08 10:50:33 -0300

    Fila Vetor Finalizado

--- projetoFila/fila.c
+++ projetoFila/fila.c
@@ -1,5 +1,88 @@
+#include "fila.h"
 /**
-*   Autora: Jaicimara Weber
-*   Data:   27/04/2015
-*   Descriçao:
-**/
+ * \author	Jaicimara Weber
+ * \date	04/05/2015
+ * \brief   Função que aloca espaço para fila.
+ * \param   void
+ * \return  Fila*
+ */
+Fila* cria (void)
+{
+    Fila* fila = (Fila*) malloc(sizeof(Fila)); /** Aloca espaço na memória **/
+    fila->inicio = fila->fim = 0;              /** Inicializa os ponteiros em vazio **/
+    return fila;                               /** Retorna fila criada **/
+}
+/**
+ * \author	Jaicimara Weber
+ * \date	08/05/2015
+ * \brief   Função que aloca espaço para fila.
+ * \param   Fila*, float
+ */
+void insere (Fila* f, float v)
+{
+    if(incr(f->fim) == f->inicio){
+        printf("Capacidade da fila estourou.\n");
+        exit(1);    /*aborta programa*/
+    }
+
+    f->vetor[f->fim] = v; /**INSERE ELEMENTO NA PROXIMA POSICAO LIVRE**/
+    f->fim = incr(f->fim);  /**INCREMENTA PROXIMA POSICAO**/
+}
+/**
+ * \author	Jaicimara Weber
+ * \date	08/05/2015
+ * \brief   Função que aloca espaço para fila.
+ * \param   Fila*
+ * \return  float
+ */
+float retira (Fila* f)
+{
+    float valor;
+
+    if(vazia(f)){
+        printf("Fila vazia.\n");
+        exit(1);
+    }
+    valor =  f->vetor[f->inicio]; /**CARREGA VALOR DO INICIO DA FILA**/
+    f->inicio = incr(f->inicio); /**INCREMENTA INDICE DO INICIO DA FILA**/
+    return valor;
+}
+
+/**
+ * \author	Jaicimara Weber
+ * \date	08/05/2015
+ * \brief   Verifica se lista está vazia
+ * \param   Fila*
+ * \return  boolean
+ */
+int vazia (Fila* f)
+{
+    return (f->inicio == f->fim);
+}
+
+/**
+ * \author	Jaicimara Weber
+ * \date	08/05/2015
+ * \brief   Verifica se lista está vazia
+ * \param   Fila*
+ */
+void libera (Fila* f)
+{
+    free(f);
+}
+
+/**
+ * \author	Jaicimara Weber
+ * \date	08/05/2015
+ * \brief   incrementa ponteiro da fila
+ * \param   int
+ * \return  int i+1
+ */
+int incr(int i)
+{
+    return (int)(i+1)% MAX_FILA;
+}
+
+
+
+
--- projetoFila/fila.h
+++ projetoFila/fila.h
@@ -1,16 +1,24 @@
 #ifndef FILA_H_INCLUDED
 #define FILA_H_INCLUDED
-/** DEFINE TAMANHO MÁXIMO DA FILA **/
-#define N 100
+/**< INCLUSAO DAS BIBLIOTECAS */
+#include <stdio.h>
+#include <stdlib.h>
+/**< DEFINE TAMANHO MÁXIMO DA FILA */
+#define MAX_FILA 5
 
-/** ESTRUTURA DA FILA - usando vetor **/
+/**< ESTRUTURA DA FILA - usando vetor */
 typedef struct fila Fila;
 
 struct fila{
-    int *fist;
-    int *last;
-    float vetor[N];
+    int inicio;
+    int fim;
+    float vetor[MAX_FILA];
 };
-
-
+/** FUNÇÕES DE FILA USANDO VETORES **/
+Fila* cria (void);
+void insere (Fila* f, float v);
+float retira (Fila* f);
+int vazia (Fila* f);
+void libera (Fila* f);
+int incr(int i);
 #endif // FILA_H_INCLUDED
